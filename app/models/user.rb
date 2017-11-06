b7fa1907fe8e23cbaef9ae08bc77657252fc2560
Catherine Rice <clr2107@gmail.com>
2017-11-06 11:51:21 -0500

    Add email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true
+
 end
