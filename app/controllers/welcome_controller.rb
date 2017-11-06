1b1cb8852551787d1e5876845f343563ef6b0165
Catherine Rice <clr2107@gmail.com>
2017-11-06 17:07:49 -0500

    Fix current_user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   include UserHelper
     def index
-      @current_recipes = @current_user.recipes
+      # binding.pry
+      # current_user = User.find_by(id: session[:user_id])
+      @recipes = current_user.recipes
+
+
     end
   end
