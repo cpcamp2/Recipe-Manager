8e9c83b22e8bcc6cb8b5bfa9e4655b95a9ac8663
Chris Camp <cpcamp2@gmail.com>
2017-11-07 11:21:21 -0500

    Fixed css bugs, avg ratings now displayed in recipe index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-    def index
-      @user = User.find_by(id: current_user.id)
-      @user_recipes = current_user.recipes
-    end
+#     def index
+#       @user = User.find_by(id: current_user.id)
+#       @user_recipes = current_user.recipes
+#     end
 
-    def show
-      @current_user
-      @user_recipes = current_user.recipes
-    end
+#     def show
+#       @current_user
+#       @user_recipes = current_user.recipes
+#     end
 
-  end
+#   end
