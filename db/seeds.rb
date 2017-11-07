7749e27538cd2871aaad1d3a0e5c978d54722619
David Skaggs <davidtskaggs@gmail.com>
2017-11-07 09:44:47 -0500

    Add navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ Recipe.delete_all
 User.delete_all
 
 # User.create(email: "clr2107@gmail.com", password: "cat")
-# penn = User.create(email: "penn@gmail.com", password: "123456")
-# catherine = User.create(email: "catherine@gmail.com", password: "123456")
-# david = User.create(email: "david@gmail.com", password: "123456")
-# amber = User.create(email: "amber@gmail.com", password: "123456")
+penn = User.create(email: "penn@gmail.com", password: "123456")
+catherine = User.create(email: "catherine@gmail.com", password: "123456")
+david = User.create(email: "david@gmail.com", password: "123456")
+amber = User.create(email: "amber@gmail.com", password: "123456")
 # cat2 = User.create(email: "clr2107@gmail.com", password: "cat")
 
 # Additional Recipes for testing
@@ -31,6 +31,6 @@ end
 # c5 = Category.create(name: "Dinner")
 # c6 = Category.create(name: "Lunch")
 
-15.times do
+30.times do
   Recipe.create(name: Faker::Food.dish, difficulty: "Hard", prep_time: Faker::Number.number(2), directions: Faker::Lorem.words, ingredients: Faker::Food.ingredient, user_id: Faker::Number.between(1, 6), category: "Appetizer")
 end
