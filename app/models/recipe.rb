a8b0a59f6080fe6c9e394e57ed05ee479ac0012d
Catherine Rice <clr2107@gmail.com>
2017-11-07 10:16:47 -0500

    Add search feature

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ratings
 
+  def self.search(search)
+    searched_recipes = []
+    @recipes.each do |recipe|
+      if recipe.ingredients.include?(search)
+        searched_recipes << recipe
+      end
+      return searched_recipes
+    end
+  end
+
 end
