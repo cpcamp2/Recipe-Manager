047a8f49d885e05010e86ba0a0492fc48e2e148f
Catherine Rice <clr2107@gmail.com>
2017-11-07 11:14:51 -0500

    User profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
     def index
-      @user = User.find_by(id: current_user.id)
+      user = User.find_by(id: current_user.id)
       @user_recipes = current_user.recipes
     end
 
     def show
-      @current_user
+      current_user
       @user_recipes = current_user.recipes
+      render 'show'
     end
 
+    def user_params
+      params.require(:user).permit(:email)
+    end
   end
