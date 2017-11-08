6b8a3a5dae8974f87cb9286cef3a3d9d7eb4ac5b
Catherine Rice <clr2107@gmail.com>
2017-11-08 09:46:30 -0500

    pry

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,7 @@
     current_user
     @recipes = Recipe.order(:name).page params[:page]
     if params[:ingredient]
-
+      binding.pry
       @recipes = Recipe.ingredient_search(params[:ingredient]).order(:name).page params[:page]
     elsif params[:name]
       @recipes = Recipe.name_search(params[:name]).order(:name).page params[:page]
